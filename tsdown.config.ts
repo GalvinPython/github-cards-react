756e0dca8644d091af4e6d1265d865d314bd08fe
Galvin <imgalvin@pm.me>
2026-04-03 10:40:46 +0100

    chore: update typescript + workflow changes (#11)
    * chore(deps-dev): bump typescript from 5.9.3 to 6.0.2 (#9)
    
    Bumps [typescript](https://github.com/microsoft/TypeScript) from 5.9.3 to 6.0.2.
    - [Release notes](https://github.com/microsoft/TypeScript/releases)
    - [Commits](https://github.com/microsoft/TypeScript/compare/v5.9.3...v6.0.2)
    
    ---
    updated-dependencies:
    - dependency-name: typescript
      dependency-version: 6.0.2
      dependency-type: direct:development
      update-type: version-update:semver-major
    ...
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * chore: migrate from tsup to tsdown
    
    * chore: update packages.json and prs workflows
    
    * add from review
    
    ---------
    
    Signed-off-by: dependabot[bot] <support@github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,11 +1,13 @@
-import { defineConfig } from 'tsup';
+import { defineConfig } from 'tsdown';
 
 export default defineConfig({
     entry: ['src/index.ts'], // Entry point of the library
     format: ['esm', 'cjs'], // Export both formats
     dts: true,             // Generate declaration files (.d.ts)
-    splitting: false,
     sourcemap: true,
-    clean: true,
-    external: ['react', 'react-dom'], // Don't bundle React
+    clean: true, // Don't bundle React
+  deps: {
+    neverBundle: ['react', 'react-dom'],
+  },
+  target: false,
 });
